f5b9f148e9407222e909251aa83133e147ed8eb0
Clinton Cecil <ccecil@ctacorp.com>
2013-10-02 16:41:37 -0500

    Added authorship to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = Post.new(post_params.merge(:user_id => current_user.id))
 
     respond_to do |format|
       if @post.save
