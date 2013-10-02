75ed0f8f552c521adf58b791106eaa281d55496b
Clinton Cecil <ccecil@ctacorp.com>
2013-10-02 14:28:25 -0500

    Added announcements to post index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
+    @announcements = Announcement.active
     @posts = Post.all(:order => 'created_at DESC')
   end
 
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,2 +1,3 @@
 class Announcement < ActiveRecord::Base
+  scope :active, where('end_date >= ?', Date.today)
 end
