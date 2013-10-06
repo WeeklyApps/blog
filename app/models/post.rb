05024043a866d71ecdf99a494feae5ef8eb75dc2
Clinton Cecil <ccecil@ctacorp.com>
2013-10-06 16:57:32 -0500

    Added tag list to index. Needs more styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,7 @@ class Post < ActiveRecord::Base
 
   self.per_page = 5
   
+  def self.tag_cloud
+    @tags = Post.tag_counts_on(:tags).limit(20).order('count desc').sort { |x, y| x.name <=> y.name }
+  end
 end
