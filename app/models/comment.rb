2c226e8917ae7acf24bb3356ad34a09af1aa8887
Clinton Cecil <ccecil@ctacorp.com>
2013-10-10 23:28:09 -0500

    Added validation to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+  
+  validates :commenter, exclusion: { in: %w[Clint Robert thatrailsguy robmc] + User.all.map{|u| u.display_name}, message: "%{value} is reserved."}
+  validates :content, presence: true
+
 end
