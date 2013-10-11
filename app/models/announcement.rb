0a3f481f0ffaef61ac9f96a910927e833b83704c
Robert McCaffery <robert.j.mccaffery@gmail.com>
2013-10-11 18:47:55 -0400

    Fixing date issue now that the american_date gem has been added

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,12 +2,7 @@ class Announcement < ActiveRecord::Base
   scope :active, where('end_date >= ?', Date.today)
   belongs_to :user
   has_many :comments, as: :commentable
-  before_save :format_date
   
   self.per_page = 15
-
-  def format_date
-    self.end_date = self.end_date.strftime('%m/%d/%Y')
-  end
   
 end
