326c329f9a4dd9f7cec13b3d5d8d619425893d8c
Robert McCaffery <robert.j.mccaffery@gmail.com>
2013-10-12 08:54:29 -0400

    Added validations for announcements

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,6 +2,9 @@ class Announcement < ActiveRecord::Base
   scope :active, where('end_date >= ?', Date.today)
   belongs_to :user
   has_many :comments, as: :commentable
+
+  validates :body, presence: true
+  validates :end_date, presence: true
   
   self.per_page = 15
   
