5641b4f27f4a3045ad66d46ac9061db6ebff86c5
Robert McCaffery <robert.j.mccaffery@gmail.com>
2013-10-12 09:12:19 -0400

    Added and styled validations for Projects and styled errors for comments

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,11 @@
 class Project < ActiveRecord::Base
   self.per_page = 15
   has_many :comments, as: :commentable
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :url, presence: true
+  validates :github_url, presence: true
+  
+
 end
